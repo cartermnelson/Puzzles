bd614bcd58313b8a3f4549504cf12390caec30ca
Carter Nelson <carternelson05@gmail.com>
2019-09-25 09:28:02 -0700

    Wrote random array function

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace Puzzles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RandomArray();
+        }
+        public static int[] RandomArray()
+        {
+            int[] arr = new int[10];
+            arr[0] = new Random().Next(5, 25);
+            int min = arr[0];
+            int max = arr[0];
+            int sum = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                arr[i] = new Random().Next(5, 25);
+                sum += arr[i];
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                else if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine($"Min: {min} | Max: {max} | Sum: {sum}");
+            return arr;
         }
     }
 }
